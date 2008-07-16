a12e94c7130cea1469f713d31a09dc04b97b68a1
Jonathan Vaught <jvaught@elevatedrails.com>
2008-07-16 09:19:43 -0400

    Help text in install

--- install.rb
+++ install.rb
@@ -2,8 +2,10 @@
 require 'fileutils'
 
 %w{ images stylesheets javascripts }.each do |dir|
-  Dir.glob "#{dir}/*" do |entry|
+  puts "Installing #{dir}"
+  Dir.glob File.join(File.dirname(__FILE__), "#{dir}/*") do |entry|
     FileUtils.cp entry, File.join(RAILS_ROOT, "public/#{dir}")
   end
 end
 
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
